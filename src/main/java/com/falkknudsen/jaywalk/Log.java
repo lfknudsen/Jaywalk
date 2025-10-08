3f7b107de98a56271cfb11c48bf3a1999e842731
Louis <louis@falkknudsen.com>
2025-10-08 22:48:16 +0200

    Added logging utility class, which only prints if the static boolean VERBOSE is true.

---
+++ src/main/java/com/falkknudsen/jaywalk/Log.java
@@ -0,0 +1,43 @@
+﻿package com.falkknudsen.jaywalk;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Log {
+    /** Print extra information to the standard output.
+     Initialise it to false here so that tests don't print said extra information. */
+    public static boolean VERBOSE = false;
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    public static void log(Exception e) {
+        Throwable cause = e.getCause();
+        if (cause != null) {
+            err("Root cause: " + cause);
+            cause.printStackTrace(System.err);
+        }
+    }
+
+    /** If {@link #VERBOSE}, print the input string to the system console along with the current
+     timestamp. */
+    public static void log(Object obj) {
+        if (VERBOSE)
+            System.out.println(LocalTime.now().format(dtf) + ": " + obj.toString());
+    }
+
+    public static void log(Object... objs) {
+        if (VERBOSE) {
+            System.out.print(LocalTime.now().format(dtf) + ":");
+            for (Object o : objs) {
+                System.out.print(" " + o.toString());
+            }
+            System.out.println();
+        }
+    }
+
+    /** If {@link #VERBOSE}, print the input string to the system error console
+     along with the current timestamp. */
+    public static void err(Object obj) {
+        if (VERBOSE)
+            System.err.println(LocalTime.now().format(dtf) + ": " + obj.toString());
+    }
+}
